e8c0ddae6da1ec00203cdad4a692db549306773d
SungHyunJi <126458483+JayJi5204@users.noreply.github.com>
2023-12-17 05:07:53 +0900

    Add files via upload

---
+++ SpringBoot/Introduce/src/main/java/Team/project/itda/Service/FormService.java
@@ -0,0 +1,12 @@
+package Team.project.itda.Service;
+
+import Team.project.itda.Repository.FormRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class FormService {
+
+    private final FormRepository fromRepository;
+}

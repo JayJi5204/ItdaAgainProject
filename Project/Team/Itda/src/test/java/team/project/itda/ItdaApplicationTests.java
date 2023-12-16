87a645ac4752ccd43d1622c96687a4601569a3c3
SungHyunJi <126458483+JayJi5204@users.noreply.github.com>
2023-12-17 02:52:47 +0900

    Add files via upload
    초기 세팅

---
+++ Project/Team/Itda/src/test/java/team/project/itda/ItdaApplicationTests.java
@@ -0,0 +1,13 @@
+package team.project.itda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItdaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

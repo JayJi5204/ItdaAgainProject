f49d2c8f57353afc69f425bcabdeb50978030d70
jayji5204 <jayji5204@naver.com>
2024-01-13 04:34:34 +0900

    로그인 사이드 바 기능 제작완료(세세한 css 미적용)

--- SpringBoot/Introduce/src/main/resources/static/JQuery/ToggleBar.js
+++ SpringBoot/Introduce/src/main/resources/static/JQuery/ToggleBar.js
@@ -1,14 +1,13 @@
-
 $(function () {
   var $aside = $('aside');
 
-  $('#cursor').click(function () {
+  $('#login').click(function () {
     $aside.toggleClass('open');
 
     if ($aside.hasClass('open')) {
-      $aside.css({ transform: 'translateX(100%)' });
+      $aside.css({ transform: 'translateX(-14rem)' });
     } else {
-      $aside.css({ transform: 'translateX(0)' });
+      $aside.css({ transform: 'translateX(0)' }); // 변경된 부분
     }
   });
 });

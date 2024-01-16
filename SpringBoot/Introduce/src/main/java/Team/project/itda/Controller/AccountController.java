d1abca7d514c0dee5689a65de2b664f2f7c473d0
jayji5204 <jayji5204@naver.com>
2024-01-17 01:12:00 +0900

    통장 페이지 정리

--- SpringBoot/Introduce/src/main/java/Team/project/itda/Controller/AccountController.java
+++ SpringBoot/Introduce/src/main/java/Team/project/itda/Controller/AccountController.java
@@ -2,7 +2,6 @@ package Team.project.itda.Controller;
 
 import Team.project.itda.DTO.AccountDTO;
 import Team.project.itda.Service.AccountService;
-import jakarta.transaction.Transactional;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
@@ -21,24 +20,53 @@ public class AccountController {
 
     private final AccountService accountService;
 
-    @GetMapping("/account")
-    public String getHomepage(Model model, Pageable pageable, Long id, Long depositMoney, String depositDetails, LocalDateTime depositTime, Long withdrawMoney, String withdrawDetails, LocalDateTime withdrawTime, Long totalMoney) {
+    @GetMapping("/account") //통장 관리 페이지
+    public String getAccountPage() {
+        return "page/AccountPage";
+    }
+    @GetMapping("/account/complete") //통장 관리 페이지
+    public String getAccountCompletePage() {
+        return "page/AccountCompletePage";
+    }
+
+    @GetMapping("/account/deposit")  //입금 페이지
+    public String getAccountDepositPage(Model model, Pageable pageable, Long id, Long depositMoney, String depositDetails, LocalDateTime depositTime, Long withdrawMoney, String withdrawDetails, LocalDateTime withdrawTime, Long totalMoney) {
+        model.addAttribute("accountSaveForm", new AccountDTO(id, depositMoney, depositDetails, depositTime, withdrawMoney, withdrawDetails, withdrawTime, totalMoney));
+        return "page/AccountDepositPage";
+    }
+
+    @PostMapping("/account/deposit") //입금 페이지
+    public String postAccountDepositPage(@Valid AccountDTO accountDTO) {
+        accountService.saveAccount(accountDTO);
+        return "redirect:/account/complete";
+    }
+
+    @GetMapping("/account/withdraw")    //출금 페이지
+    public String getAccountWithdrawPage(Model model, Pageable pageable, Long id, Long depositMoney, String depositDetails, LocalDateTime depositTime, Long withdrawMoney, String withdrawDetails, LocalDateTime withdrawTime, Long totalMoney) {
         Page<AccountDTO> accountDTOPage = accountService.getAccount(pageable);
         model.addAttribute("accountForm", accountDTOPage);
         model.addAttribute("accountSaveForm", new AccountDTO(id, depositMoney, depositDetails, depositTime, withdrawMoney, withdrawDetails, withdrawTime, totalMoney));
-        return "page/AccountPage";
+        return "page/AccountWithdrawPage";
     }
 
-    @PostMapping("/account")
-    public String postStartGame(@Valid AccountDTO accountDTO) {
+    @PostMapping("/account/withdraw")   //출금 페이지
+    public String postAccountWithdrawPage(@Valid AccountDTO accountDTO) {
         accountService.saveAccount(accountDTO);
-        return "redirect:/account";
+        return "redirect:/account/complete";
+    }
+
+
+    @GetMapping("/account/detail")  //관리 페이지
+    public String getAccountDetailPage(Model model, Pageable pageable) {
+        Page<AccountDTO> accountDTOPage = accountService.getAccount(pageable);
+        model.addAttribute("accountForm", accountDTOPage);
+        return "page/AccountDetailPage";
     }
 
     @PostMapping("/account/{id}/delete")
     public String deleteAccount(@PathVariable Long id) {
         accountService.deleteAccount(id);
-        return "redirect:/account";
+        return "redirect:/account/complete";
     }
 
 }

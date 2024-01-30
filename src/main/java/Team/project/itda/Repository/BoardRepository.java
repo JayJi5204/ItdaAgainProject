a09178547bb491ba1942b61725295fd3952e4d64
snueg <ssnueg@gmail.com>
2024-01-30 17:58:58 +0900

    hs 수정삭제

--- src/main/java/Team/project/itda/Repository/BoardRepository.java
+++
@@ -1,9 +0,0 @@
-package Team.project.itda.Repository;
-
-
-import Team.project.itda.Entity.Board;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.querydsl.QuerydslPredicateExecutor;
-
-public interface BoardRepository extends JpaRepository<Board, Long>, QuerydslPredicateExecutor<Board> {
-}

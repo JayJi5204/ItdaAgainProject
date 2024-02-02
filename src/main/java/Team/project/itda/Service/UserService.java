00475840493dd20f720d9dec330542bf4126b1b2
allready-dawn <qwshpke@naver.com>
2024-02-02 15:32:45 +0900

    유저 뷰 중앙, 김상준 후기

--- src/main/java/Team/project/itda/Service/UserService.java
+++ src/main/java/Team/project/itda/Service/UserService.java
@@ -3,19 +3,11 @@ package Team.project.itda.Service;
 import Team.project.itda.DTO.UserFormDTO;
 import Team.project.itda.DTO.UserUpdateFormDTO;
 import Team.project.itda.Entity.UserEntity;
-import Team.project.itda.Entity.UserRole;
 import Team.project.itda.Repository.UserRepository;
-import jakarta.validation.constraints.Null;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.log4j.Log4j2;
-import org.springframework.security.access.AccessDeniedException;
-import org.springframework.security.core.userdetails.User;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-
-import java.util.NoSuchElementException;
-import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -50,7 +42,7 @@ public class UserService {
 
     // 유저정보 비밀번호 수정
     @Transactional
-    public void update(Long id, UserUpdateFormDTO updateFormDTO){
+    public void update(Long id, UserUpdateFormDTO updateFormDTO) {
         UserEntity userEntity = userRepository.findById(id).orElseThrow(() ->
                 new IllegalArgumentException("해당 회원이 존재하지 않습니다."));
 
@@ -64,7 +56,7 @@ public class UserService {
     }
 
     // 패스워드 체크
-    public boolean isChkPassword(String currPassword, String chkPassword){
+    public boolean isChkPassword(String currPassword, String chkPassword) {
 
         return bCryptPasswordEncoder.matches(chkPassword, currPassword);
     }
@@ -72,11 +64,7 @@ public class UserService {
     // 로그인한 유저와 데이터 작성한 유저가 같은지 검증
     public boolean isChkUser(Long pathId, Long userId) {
 
-        if(!pathId.equals(userId)) {
-            return false;
-        }
-
-        return true;
+        return pathId.equals(userId);
     }
 
 }
